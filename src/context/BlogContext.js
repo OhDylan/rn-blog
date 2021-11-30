8b6330c06ec179cb1d5dc33d7cb401b3189b5e22
Oh Dylan <ddragon1128@hotmail.com>
2021-11-30 21:34:29 +0800

    Refactored code for future maintenance

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch(action.type) {
@@ -11,20 +10,12 @@ const blogReducer = (state, action) => {
     }
 } 
 
-export const BlogProvider = ({children}) => {
-
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-    const addBlogPost = () => {
-        dispatch({type: 'add_blogpost'});
-    }
-
-    return (
-        <BlogContext.Provider value={{
-            data: blogPosts,
-            addBlogPost
-        }}>
-            {children}
-        </BlogContext.Provider>
-    );
+const addBlogPost = () => {
+    dispatch({type: 'add_blogpost'});
 };
+
+export const { Context, Provider } = createDataContext(
+    blogReducer, 
+    {addBlogPost},
+    []
+);
