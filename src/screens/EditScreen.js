5646cff06eb12da980c5881cfa0a989dd32546e4
Oh Dylan <ddragon1128@hotmail.com>
2021-12-04 11:58:07 +0800

    Extracting the form logic out to a component

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,30 +1,20 @@
-import React, { useState, useContext } from 'react';
-import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({navigation}) => {
 
+    const { state } = useContext(Context);
+    const blogPost = state.find(blogPost => blogPost.id === navigation.getParan('id'));
+
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <BlogPostForm />
     );
 };
 
 const styles = StyleSheet.create({
-    input: {
-        fontSize: 8,
-        borderWidth: 1, 
-        borderColor: 'black',
-        marginBottom: 15,
-        padding: 5,
-        margin: 5
-    },
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5
-    }
+    
 });
 
 export default EditScreen;
